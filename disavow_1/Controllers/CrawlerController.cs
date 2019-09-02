b6c5618db0461f350c6b1173d2cf232e77ae5aa3
larryhpham <larrypham1991@yahoo.com>
2019-09-02 03:43:00 -0500

    added post api

--- disavow_1/Controllers/CrawlerController.cs
+++ disavow_1/Controllers/CrawlerController.cs
@@ -4,16 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using disavow_1.Models;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace disavow_1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("disavow/[controller]")]
     [ApiController]
     public class CrawlerController : ControllerBase
     {
@@ -34,9 +36,14 @@ namespace disavow_1.Controllers
             {
                 return NotFound();
             }
+            Console.WriteLine($"Looking into site:{item.Url}");
 
-            GetHtmlAsync("https://www.victoriavanle.com/", "magneticsketch");
+            // TODO: need to add pattern to enter input into frontend and store that in DB
 
+            // TODO: get input from FRONTEND to fil in Pattern in db to replace www.magneticsketch
+            GetHtmlAsync(item.Url, "www.magneticsketch");
+
+            // TODO: Return not the item but a better reponse for post call
             return item;
         }
 
@@ -54,11 +61,44 @@ namespace disavow_1.Controllers
             foreach (var tnodes in htmlNodes)
             {
                 var href = tnodes.GetAttributeValue("href", "");
-                Console.WriteLine("href:" + href + "=>" + IsValid(href, search_pattern));
+                if(IsValid(href, search_pattern))
+                {
+                    Console.WriteLine("Get list of pages from ahref.com");
+                    List<Pages> pages = GetThirdPartyAPI(href);
+
+                    if (pages != null)
+                    {
+                        Console.WriteLine("Change page rankings if below threshold then flag");
+                        foreach (Pages page in pages)
+                        {
+                            if(page.ahrefs_rank < 50)
+                            {
+                                Console.WriteLine($"\nsite:{page.url}");
+                                Console.WriteLine($"\nBAD RATING of {page.ahrefs_rank} need to flag as bad site");
+                            }
+                        }
+                    }
+                }
 
             }
         }
 
+        private static List<Pages> GetThirdPartyAPI(string url)
+        {
+            var thirdPartyApi = "https://apiv2.ahrefs.com/?token=5e939df83301fe6711378e35648802428c0c03a5&limit=100&output=json&from=ahrefs_rank&mode=exact";
+            thirdPartyApi += "&target=" + url;
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = httpClient.GetAsync(thirdPartyApi).Result;
+            string stringData = response.Content.ReadAsStringAsync().Result;
+
+            TestApi data = JsonConvert.DeserializeObject<TestApi>(stringData);
+            List<Pages> urls = data.pages;
+
+            return urls;
+        }
+
+        // Checks for bad urls
         static bool IsValid(string value, string pattern)
         {
             return Regex.IsMatch(value, pattern);
---
+++ disavow_1/Models/TestApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace disavow_1.Models
+{
+    public class TestApi
+    {
+        public List<Pages> pages { get; set; }
+    }
+
+    public class Pages
+    {
+        public string url { get; set; }
+        public int ahrefs_rank { get; set; }
+    }
+}
